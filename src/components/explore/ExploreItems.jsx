7445df39ddc9c1f07e19cc13ca25b6b34c88f70f
RosyCenteio <rosynirabarbosa@gmail.com>
2026-03-22 18:32:40 -0400

    create Explore Page Dynamically

--- src/components/explore/ExploreItems.jsx
+++ src/components/explore/ExploreItems.jsx
@@ -1,78 +1,124 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
+import Skeleton from "../UI/Skeleton";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import axios from "axios";
 
-const ExploreItems = () => {
-  return (
-    <>
-      <div>
-        <select id="filter-items" defaultValue="">
-          <option value="">Default</option>
-          <option value="price_low_to_high">Price, Low to High</option>
-          <option value="price_high_to_low">Price, High to Low</option>
-          <option value="likes_high_to_low">Most liked</option>
-        </select>
-      </div>
-      {new Array(8).fill(0).map((_, index) => (
+const ExploreItems = ({explorePage,index, item, loading, onFilterChange, onLoadMore, visibleCount}) => {
+
+  function getTimeLeft(date) {
+    const now = new Date();
+    const expiry = new Date(date);
+
+    const diff = expiry - now;
+
+    if (diff <= 0){
+      return null;
+    } 
+    const hours = Math.floor(diff / (1000 * 60 * 60));
+    const minutes = Math.floor(diff / (1000 * 60)) % 60;
+    const seconds = Math.floor(diff / 1000) % 60;
+    return`${hours}h ${minutes}m ${seconds}s`;
+  }
+
+    function getHtmlItem(item, index) {
+      return (
         <div
-          key={index}
-          className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
-          style={{ display: "block", backgroundSize: "cover" }}
-        >
-          <div className="nft__item">
-            <div className="author_list_pp">
-              <Link
-                to="/author"
-                data-bs-toggle="tooltip"
-                data-bs-placement="top"
-              >
-                <img className="lazy" src={AuthorImage} alt="" />
-                <i className="fa fa-check"></i>
-              </Link>
-            </div>
-            <div className="de_countdown">5h 30m 32s</div>
+        key={index}
+        className={explorePage ? "d-item col-lg-3 col-md-6 col-sm-6 col-xs-12" : "collection-slide"}
+        style={{ display: "block", backgroundSize: "cover" }}>
+        <div className="nft__item">
+          <div className="author_list_pp">
+            <Link
+              to={`/author/${item.authorId}`}
+              data-bs-toggle="tooltip"
+              data-bs-placement="top"
+              title={`Creator: ${item.authorName}`}
+            >
+              <img className="lazy" src={item.authorImage} alt="" />
+              <i className="fa fa-check"></i>
+            </Link>
+          </div>
 
-            <div className="nft__item_wrap">
-              <div className="nft__item_extra">
-                <div className="nft__item_buttons">
-                  <button>Buy Now</button>
-                  <div className="nft__item_share">
-                    <h4>Share</h4>
-                    <a href="" target="_blank" rel="noreferrer">
-                      <i className="fa fa-facebook fa-lg"></i>
-                    </a>
-                    <a href="" target="_blank" rel="noreferrer">
-                      <i className="fa fa-twitter fa-lg"></i>
-                    </a>
-                    <a href="">
-                      <i className="fa fa-envelope fa-lg"></i>
-                    </a>
-                  </div>
+          {getTimeLeft(item.expiryDate) && (
+          <div className="de_countdown">
+            {getTimeLeft(item.expiryDate)}
+          </div>
+        )}
+
+          <div className="nft__item_wrap">
+            <div className="nft__item_extra">
+              <div className="nft__item_buttons">
+                <button>Buy Now</button>
+                <div className="nft__item_share">
+                  <h4>Share</h4>
+                  <a href="" target="_blank" rel="noreferrer">
+                    <i className="fa fa-facebook fa-lg"></i>
+                  </a>
+                  <a href="" target="_blank" rel="noreferrer">
+                    <i className="fa fa-twitter fa-lg"></i>
+                  </a>
+                  <a href="">
+                    <i className="fa fa-envelope fa-lg"></i>
+                  </a>
                 </div>
               </div>
-              <Link to="/item-details">
-                <img src={nftImage} className="lazy nft__item_preview" alt="" />
-              </Link>
             </div>
-            <div className="nft__item_info">
-              <Link to="/item-details">
-                <h4>Pinky Ocean</h4>
-              </Link>
-              <div className="nft__item_price">1.74 ETH</div>
-              <div className="nft__item_like">
-                <i className="fa fa-heart"></i>
-                <span>69</span>
-              </div>
+            <Link to="/item-details">
+              <img
+                src={item.nftImage}
+                className="lazy nft__item_preview"
+                alt=""
+              />
+            </Link>
+          </div>
+          <div className="nft__item_info">
+            <Link to="/item-details">
+              <h4>{item.title}</h4>
+            </Link>
+            <div className="nft__item_price">{item.price} ETH</div>
+            <div className="nft__item_like">
+              <i className="fa fa-heart"></i>
+              <span>{item.likes}</span>
             </div>
           </div>
         </div>
-      ))}
-      <div className="col-md-12 text-center">
-        <Link to="" id="loadmore" className="btn-main lead">
-          Load more
-        </Link>
-      </div>
+      </div>)
+    }
+
+
+
+  return (
+    <>
+      {explorePage &&
+        <div>
+          <select id="filter-items" defaultValue="" onChange={(e) => onFilterChange(e.target.value)}>
+            <option value="">Default</option>
+            <option value="price_low_to_high">Price, Low to High</option>
+            <option value="price_high_to_low">Price, High to Low</option>
+            <option value="likes_high_to_low">Most liked</option>
+          </select>
+        </div>
+      }
+      {!loading ? (
+          explorePage ? item.slice(0, visibleCount).map((item, index) => getHtmlItem(item, index)) :  getHtmlItem(item, index)
+          ) : (
+
+          explorePage ? 
+            item.slice(0, visibleCount).map((item, index)=> (
+            <Skeleton key={index} width="228px" height="300px" borderRadius="10px" marginBottom="20px"></Skeleton>)
+            ) : (
+              <Skeleton key={index} width="228px" height="300px" borderRadius="10px" marginBottom="20px"></Skeleton>)
+          )} 
+        { explorePage && visibleCount < item.length && 
+          <div className="col-md-12 text-center">
+            <button onClick={onLoadMore} className="btn-main lead">
+              Load more
+            </button>
+          </div>
+        }
     </>
   );
 };
